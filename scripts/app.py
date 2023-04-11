8a82132808b8a1b77679ef4a376d8cb5826bb3f4
ricardo <ricardo.rodrigues@hoverture.com>
2023-04-11 14:12:08 +0100

    fix flask and js.html

--- scripts/app.py
+++ scripts/app.py
@@ -42,7 +42,8 @@ def main():
     return render_template('js.html')
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', debug=True)
+    socketio.run(app, host='0.0.0.0', debug=True, allow_unsafe_werkzeug=True)
+
 
 
 
