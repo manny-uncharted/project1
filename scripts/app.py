e564aeb30bdda818b4153538eee0b7e97ff35ef4
ricardo <ricardo.rodrigues@hoverture.com>
2023-04-11 13:41:58 +0100

    fix flask and js.html

--- scripts/app.py
+++ scripts/app.py
@@ -2,21 +2,21 @@ from flask import Flask, request, jsonify, render_template
 from multiprocessing import Process
 import subprocess
 import os
+import time
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__, template_folder=os.path.abspath('templates'))
-
-def start_background_process(openai_api_key, eleven_labs_api_key):
-    env = {'OPENAI_API_KEY': openai_api_key, 'ELEVEN_LABS_API_KEY': eleven_labs_api_key}
-
-    try:
-        process = subprocess.Popen(['screen', '-S', 'my_screen', '-dm', 'python', './scripts/main.py'], env=env)
-        print(f'Subprocess started with PID {process.pid}')
-    except FileNotFoundError:
-        print('File not found')
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 @app.route('/')
 def hello_world():
     return 'Welcome to the Afterflea!'
+
+@app.route('/run', methods=['GET', 'POST'])
+import webbrowser
+
+...
 
 @app.route('/run', methods=['GET', 'POST'])
 def run_app():
@@ -29,12 +29,24 @@ def run_app():
     if not openai_api_key or not eleven_labs_api_key:
         return jsonify({"error": "API keys are required."}), 400
 
-    p = Process(target=start_background_process, args=(openai_api_key, eleven_labs_api_key))
-    p.start()
+    env = {'OPENAI_API_KEY': openai_api_key, 'ELEVEN_LABS_API_KEY': eleven_labs_api_key}
+
+    try:
+        subprocess.Popen(['python', './scripts/main.py'], env=env)
+        print('Subprocess started.')
+    except FileNotFoundError:
+        return jsonify({"error": "File not found."}), 500
+
+    webbrowser.open_new_tab('http://localhost:5000/main')
 
     return jsonify({"result": "App is starting up."})
 
+@app.route('/main')
+def main():
+    return render_template('js.html')
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    socketio.run(app, host='0.0.0.0', debug=True)
+
 
 
